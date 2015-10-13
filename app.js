94061e6221a2924e07749df1f1f0cd87e03d77ce
yazanealaily <yazane.alaily@gmail.com>
2015-10-13 21:54:49 +0300

    add leaflet map

--- app.js
+++ app.js
@@ -8,20 +8,38 @@
 		var location = "Beirut, LB";
 		var resultsLimit = 50;
 		var urlAPI = "https://api.foursquare.com/v2/venues/search?";
+		var map;
+
+		mapInit();
 
 		$("#userSubmit").on("click", function(event) {
 			event.preventDefault();
 			fourSquareAPI(userSearch());
 		});
 
-			
+		//Initialize Beirut Map
+
+		function mapInit() {
+
+		 	map = L.map('map').setView([33.8869, 35.5131], 13);	
+
+			L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    		maxZoom: 18,
+    		id: 'yazane.nmm78g7o',
+    		accessToken: 'pk.eyJ1IjoieWF6YW5lIiwiYSI6IjM1Y2E1NGJjZWQ3ZmVmOWRjYzAxODdjYzA3ZjdmNWU2In0.NkpjIbCDddYfY0Ddvdy_9A'
+			}).addTo(map);	
+		}
+
+		//Retrieve User Query
 
 		function userSearch(){
 
 				var query = $("#userQuery").val();
 				return query;
+			}
 
-			};
+		//Search the FourSquare API for User Query and Display Results on Map	
 
 		function fourSquareAPI(query) {
 	
@@ -29,7 +47,18 @@
 			url: "https://api.foursquare.com/v2/venues/search?query=" + query + "&near=" + location + "&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20151010" + "&intent=browse" + "&limit=" + resultsLimit,
 			dataType: "jsonp",
 			type: "GET"
+			}).done(function(results){
+			console.log(results);
+			$.each(results.response.venues, function(i, venue) {
+
+				var latitude = parseFloat(venue.location.lat);
+				var longitude = parseFloat(venue.location.lng);
+				console.log(venue);
+				L.marker([latitude, longitude]).addTo(map).bindPopup("<b>" + venue.name + "</b>").openPopup();
+				})
 			})
+			
+			.fail(function(){console.log("Failed!");});
 		}
 
 	})
